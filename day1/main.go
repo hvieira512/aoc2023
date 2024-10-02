02e555874943b4eb1d7f00dd85aed478490aaa5b
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-10-02 18:09:51 +0100

    Day 1 Part 1

--- day1/main.go
+++ day1/main.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+func getPart1(data []byte) int {
+	result := 0
+	numberLineList := []string{}
+
+	for _, char := range string(data) {
+		if unicode.IsDigit(char) {
+			numberLineList = append(numberLineList, string(char))
+		}
+
+		if unicode.IsSpace(char) {
+			lastPos := len(numberLineList) - 1
+			strAux := string(numberLineList[0]) + string(numberLineList[lastPos])
+
+			lineNumber, err := strconv.Atoi(strAux)
+			if err != nil {
+				log.Fatal(err)
+			}
+			result = result + lineNumber
+
+			// Reset for next line
+			numberLineList = []string{}
+		}
+	}
+
+	return result
+}
+
+func getPart2(data []byte) int {
+	result := 0
+	numberLineList := []string{}
+
+	for _, char := range string(data) {
+		if unicode.IsDigit(char) {
+			numberLineList = append(numberLineList, string(char))
+		}
+
+		if unicode.IsSpace(char) {
+			lastPos := len(numberLineList) - 1
+			strAux := string(numberLineList[0]) + string(numberLineList[lastPos])
+
+			lineNumber, err := strconv.Atoi(strAux)
+			if err != nil {
+				log.Fatal(err)
+			}
+			result = result + lineNumber
+
+			// Reset for next line
+			numberLineList = []string{}
+		}
+	}
+
+	return result
+}
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resultPart1 := getPart1(data)
+	fmt.Printf("Part 1: %d\n", resultPart1)
+}
